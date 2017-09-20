2dca224e45ee31ec79407cde11802864b6ca6bbd
AlexBrady <a-brady-9@hotmail.com>
2017-09-20 16:55:40 +0100

    initial version

---
+++ calculator.py
@@ -0,0 +1,30 @@
+#######################################
+# Basic arithmetic calculator program #
+#           LAB 1 PART C              #
+#######################################
+
+import tkinter as tk
+import math
+
+class Calculator(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.QUIT = tk.Button(self, text="QUIT", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Calculator(master=root)
+app.mainloop()
